c01082d0cb70cbac38923e9fe5bc9bf7920cb45a
Pietro Malerba <pietro.malerba@gmail.com>
2011-03-31 22:43:08 +0200

    removing publib file. Use the other private repo

--- tinycms/admin/db_con.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connection=mysql_connect($dbhost,$dbuser,$dbpass);
-mysql_select_db($dbname,$connection) or die (mysql_error());
-?>
--- tinycms/fbtab/header.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<head>
-<link rel="stylesheet" href="style.css"/>
-</head>
